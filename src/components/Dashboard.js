e5cfc0f91438670a554989c236eea7d1d3d20dc8
rutik wankhade <rutikwankhade2@gmail.com>
2021-04-13 21:33:10 +0530

    add dashboard after login to app

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,11 @@
 import React from 'react';
 const Dashboard = () => {
     return ( 
-        <div>
-            <h1>Dasshboard</h1>
+        <div className="">
+            <div className="bg-indigo-500 text-xl text-white p-4">
+<h1 className="text-2xl font-semibold px-20">Bucket</h1>
+            </div>
+            <h1 className="text-center mt-60 text-4xl font-bold italic">📃 Create your Bucket list and 🎉 fulfill your dreams</h1>
         </div>
      );
 }
