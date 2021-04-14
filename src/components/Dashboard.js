838b7f5c3d111a259a0b22c13bad9189af18540a
rutik wankhade <rutikwankhade2@gmail.com>
2021-04-14 18:44:03 +0530

    feat: add data to firebase

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import avatar from '../assets/avatar.svg'
 import { useAuth } from '../contexts/AuthContext'
 import { useHistory } from 'react-router-dom'
@@ -10,7 +10,10 @@ const Dashboard = () => {
     const [error, setError] = useState("")
     const { currentUser, logout } = useAuth();
     const listRef = firestore.collection(`users/${currentUser.uid}/list`)
-    const [list] = useCollectionData(listRef)
+    const [list] = useCollectionData(listRef);
+
+    const titleRef = useRef();
+    const descRef = useRef();
 
     console.log(currentUser)
     const history = useHistory()
@@ -23,17 +26,24 @@ const Dashboard = () => {
         } catch {
             setError('Failed to logout')
         }
-
     }
 
 
 
 
-    const handleSubmitItem = () => {
+    const handleSubmitItem = (e) => {
+        e.preventDefault();
+
         listRef.add({
-            text: 'hellllo'
+            title: titleRef.current.value,
+            description: descRef.current.value,
+            completed:false,
+            createdAt: firestore.FieldValue.serverTimestamp()
+
         })
     }
+
+
 
 
     return (
@@ -49,13 +59,15 @@ const Dashboard = () => {
                     <img src={avatar} alt="avatar" className="w-24 mx-auto mt-12 border-4 border-white rounded-full" />
                     <h2 className="text-xl text-center mt-6 bg-pink-200 px-6 py-1 rounded-full m-4">{currentUser.email}</h2>
 
-                    
-                        <form onSubmit={handleSubmitItem} className="flex flex-col">
-                            <input placeholder="What's something you always wanted to do?" className="w-96 mt-2 p-2 border-2 rounded outline-none" />
-                            <textarea placeholder="Tell us more!" className="mt-2 p-2 h-40 border-2 rounded outline-none" />
-                            <button className="text-white text-xl bg-indigo-400 hover:bg-indigo-500 rounded p-2 m-2">Add to my Bucket List</button>
-                        </form>
-                    
+
+                    <form onSubmit={handleSubmitItem} className="flex flex-col">
+                        <input placeholder="What's something you always wanted to do?" ref={titleRef}
+                            className="w-96 mt-2 p-2 border-2 rounded outline-none" />
+                        <textarea placeholder="Tell us more!" ref={descRef}
+                            className="mt-2 p-2 h-40 border-2 rounded outline-none" />
+                        <button className="text-white text-xl bg-indigo-400 hover:bg-indigo-500 rounded p-2 m-2">Add to my Bucket List</button>
+                    </form>
+
                 </div>
                 <div className="overflow-y-scroll">
                     {error && <span className="bg-red-100 p-2 m-4">{error}</span>}
