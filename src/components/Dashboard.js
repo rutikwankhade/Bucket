1e44a3f4b08ba766dd2261e09b798dd8d63919b5
rutik wankhade <rutikwankhade2@gmail.com>
2021-09-30 17:10:36 +0530

    update ui and remove unused code

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,6 @@ import closeIcon from '../assets/close.svg'
 
 
 import { useAuth } from '../contexts/AuthContext'
-// import { useHistory } from 'react-router-dom'
 import { firestore } from '../firebase'
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
@@ -27,9 +26,6 @@ const Dashboard = () => {
     // const allListRef = firestore.collection(`bucketlist`)
     // const [AllList] = useCollectionData(allListRef);
 
-    // console.log(currentUser)
-    // const history = useHistory()
-
 
 
     const handleSubmitItem = (e) => {
@@ -42,15 +38,6 @@ const Dashboard = () => {
             // createdAt: firestore.FieldValue.serverTimestamp()
 
         })
-
-        // allListRef.add({
-        //     title: titleRef.current.value,
-        //     description: descRef.current.value,
-        //     completed: false,
-        //     user:currentUser.uid
-        // })
-
-
 
         titleRef.current.value = ""
     }
@@ -74,21 +61,40 @@ const Dashboard = () => {
                     {/* <img src={currentUser.photoURL || avatar} alt="avatar" className="w-24 mx-auto mt-12 border-4 border-white rounded-full" />
                     <h1 className="text-2xl text-center  py-1  ">{currentUser.displayName}</h1> */}
 
-                    {/* <h2 className="text-xl text-center mt-6 bg-pink-200 px-6 py-1 rounded-full m-4">{currentUser.email}</h2> */}
 
-                    <div className="w-full border-2 bg-white transform translate-y-12 py-10   rounded-xl  ">
+                    <div className="w-full border-2 bg-white transform translate-y-10 py-4   rounded-xl  ">
                         <h1 className="text-6xl text-center font-bold">{list && list.length}</h1>
-                        <p className="text-xl text-center">goals in your bucket list.</p>
+                        <p className="text-xl text-center">Total goals in your bucket list.</p>
+
+                        <div className="flex items-center justify-between px-10 pt-4">
+
+                            <div className="flex items-center border p-2 rounded">
+                                <span className="text-2xl font-bold mx-2 text-green-400">
+                                    {list && list.filter((wish) => wish.completed === false).length}
+                                </span>
+                                <span>Remaining</span>
+                            </div>
+
+                            <div className="flex items-center border p-2 rounded">
+                                <span className="text-2xl font-bold mx-2 text-red-400">
+                                    {list && list.filter((wish) => wish.completed === true).length}
+                                </span>
+                                <span>Completed</span>
+                            </div>
+
+
+                        </div>
+
+
                     </div>
-                    <form onSubmit={handleSubmitItem} className="flex border-2 flex-col bg-white pt-20 p-4 py-10 rounded">
+                    <form onSubmit={handleSubmitItem} className="flex border-2 flex-col bg-white pt-20 px-6 py-10 rounded">
 
 
                         <h1 className="text-2xl font-semibold text-center my-4">What's something you always wanted to do?</h1>
 
                         <input required placeholder="What's on your list?" ref={titleRef}
-                            className="text-xl w-96	 my-2 p-2 border-2 rounded outline-none" />
-                        {/* <textarea required placeholder="Tell us more!" ref={descRef}
-                            className="text-xl mt-2 p-2 h-40 border-2 rounded outline-none" /> */}
+                            className="text-xl w-full	 my-2 p-2 border-2 rounded outline-none" />
+
 
                         <select
                             onChange={(e) => setCategory(e.target.value)}
@@ -113,8 +119,6 @@ const Dashboard = () => {
 
 
                 <div className="px-10 py-4 md:-8/12 ml-auto mr-2">
-                    {/* {error && <span className="bg-red-100 p-2 m-4">{error}</span>} */}
-                    {/* <h1 className="text-center  text-2xl font-bold italic">📃 Create your Bucket list and 🎉 fulfill your dreams</h1> */}
 
 
                     <div className="bg-white p-4 rounded flex text-center items-center justify-center">
@@ -197,24 +201,22 @@ const Dashboard = () => {
                                     .map(wish => {
                                         return (
                                             <div key={wish.id}
-                                                className={`bg-white shadow-sm border rounded-md w-10/12  flex flex-row ${wish.completed === false ? 'border-green-50 ' : 'border-red-50'}`}>
+                                                className={`bg-white shadow-sm border rounded-md mx-4  flex flex-row ${wish.completed === false ? 'border-green-50 ' : 'border-red-50'}`}>
 
                                                 <div className="p-2 px-6 flex items-center">
                                                     <input type="checkbox"
                                                         checked={wish.completed}
-                                                        
+
                                                         onClick={() => handleCompleteWish(wish.id, wish.completed)}
                                                         className="text-green-600  h-4 w-4 mr-6" />
 
-                                                    <h1 className={`text-xl font-semibold ${wish.completed===true?'line-through text-gray-600':''}`}>{wish.title}</h1>
-                                                    {/* <p className="mt-2 italic">{wish.description}</p> */}
+                                                    <h1 className={`text-xl font-semibold ${wish.completed === true ? 'line-through text-gray-600' : ''}`}>{wish.title}</h1>
                                                 </div>
 
                                                 <div className="ml-auto mr-2 flex flex-row items-center">
                                                     <img src={closeIcon} alt="delete" onClick={() => handleDeleteWish(wish.id)}
                                                         className="bg-gray-50 hover:bg-pink-100 rounded-full p-1 w-8 h-8  mt-2 mb-auto cursor-pointer" />
-                                                    {/* <button onClick={() => handleCompleteWish(wish.id, wish.completed)}
-                                                className="mb-2 mt-auto bg-indigo-400 text-white px-4 p-1 rounded ">Done</button> */}
+
                                                 </div>
 
                                             </div>
